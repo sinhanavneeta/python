e06e5c265c326856e80aca880b6032c2924d75b1
sinhanavneeta <111167352+sinhanavneeta@users.noreply.github.com>
2022-08-22 16:32:24 +0530

    Create substraction.py

---
+++ substraction.py
@@ -0,0 +1,19 @@
+"""
+ Subtract 2 numbers and print result program.
+	- Write down all scenarios with different input values
+"""
+print(23 - 12)
+# static initialization
+a = 65
+b = 21
+print("value of a - b is: ", a - b)
+c = a - b
+print(c)
+# dynamic initialization
+a = int(input("Please enter first number: "))
+b = int(input("Please enter second number: "))
+print("value of a - b is: ", a - b)
+
+x, y, z = [10, 45, 21]
+print(x, y, z)
+
